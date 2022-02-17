4c94493fbbd10e642c7fb7a48192c1f0428d1964
Karena Kreger <karenalenore@gmail.com>
2022-02-16 19:40:38 -0500

    Update class-fl-child-theme.php

--- classes/class-fl-child-theme.php
+++ classes/class-fl-child-theme.php
@@ -6,14 +6,21 @@
  * @class FLChildTheme
  */
 final class FLChildTheme {
-    
-    /**
+	
+	/**
 	 * Enqueues scripts and styles.
 	 *
-     * @return void
-     */
-    static public function enqueue_scripts()
-    {
-	    wp_enqueue_style( 'fl-child-theme', FL_CHILD_THEME_URL . '/style.css' );
-    }
+	 * @return void
+	 */
+	static public function enqueue_scripts()
+	{
+		$version  = date('ymd-Gis', filemtime( FL_CHILD_THEME_DIR . '/style.css' ));
+		wp_enqueue_style( 'fl-child-theme', FL_CHILD_THEME_URL . '/style.css', array(), $version  );
+
+		$version  = date('ymd-Gis', filemtime( FL_CHILD_THEME_DIR . '/print.css' ));
+		wp_enqueue_style( 'print', FL_CHILD_THEME_URL . '/print.css', array(), $version );
+
+		$version  = date('ymd-Gis', filemtime( FL_CHILD_THEME_DIR . '/custom.js' ));
+		wp_enqueue_script( 'custom', FL_CHILD_THEME_URL . '/custom.js', array(), $version );
+	}
 }
